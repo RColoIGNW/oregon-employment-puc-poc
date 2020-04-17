0e91dc130a1293e1c6c5e516c20d47bef723df57
desmondpnw3dev <pnw3dev@gmail.com>
2020-04-17 09:07:47 -0700

    types

--- oed-server/src/util/token.ts
+++ oed-server/src/util/token.ts
@@ -1,72 +1,73 @@
-// Import Firebase Admin initialized instance to middleware
-import firebase from './firebase';
+import { NextFunction, Request, Response } from 'express'
 
-const roleRanks = {
-  superAdmin: 1,
-  admin: 2,
-  user: 3
-};
+import firebase from './firebase'
 
-export const decodeFirebaseIdToken = async (req, res, next) => {
+// const roleRanks = {
+//   superAdmin: 1,
+//   admin: 2,
+//   user: 3
+// }
+
+export const decodeFirebaseIdToken = async (req: Request|any, res: Response, next: NextFunction) => {
   if (!req.headers.id_token) {
     return res.status(400).json({
       error: {
         message: 'You did not specify any idToken for this request'
       }
-    });
+    })
   }
 
   try {
     // Use firebase-admin auth to verify the token passed in from the client header.
     // This is token is generated from the firebase client
     // Decoding this token returns the userpayload and all the other token claims you added while creating the custom token
-    const userPayload = await firebase.auth().verifyIdToken(req.headers.id_token);
+    const userPayload = await firebase.auth().verifyIdToken(req.headers.id_token)
 
-    req.user = userPayload;
+    req.user = userPayload
 
-    next();
+    next()
   } catch (error) {
     return res.status(500).json({
       error
-    });
+    })
   }
-};
+}
 
 // Checks if a user is authenticated from firebase admin
-export const isAuthorized = async (req, res, next) => {
+export const isAuthorized = async (req: Request|any, res, next) => {
   if (req.user) {
-    next();
+    next()
   } else {
     return res.status(401).json({
       error: {
         message: 'You are not authorised to perform this action. SignUp/Login to continue'
       }
-    });
+    })
   }
-};
+}
 
 // Checks if a user has the required permission from token claims stored in firebase admin for the user
-export const hasAdminRole = async (req, res, next) => {
+export const hasAdminRole = async (_: Request, res, next) => {
   try {
-    const roleRequest = await firebase.database().ref('roles').once('value');
-    const rolesPayload = roleRequest.val();
-    const role = rolesPayload.find((role) => role.id === roleRanks.admin)
+    // const roleRequest = await firebase.database().ref('roles').once('value')
+    // const rolesPayload = roleRequest.val()
+    // const role = rolesPayload.find((role) => role.id === roleRanks.admin)
 
     next()
     // if (req.user.roleId <= role.id) {
-    //   next();
+    //   next()
     // } else {
     //   return res.status(403).json({
     //     error: {
     //       message: 'You are not permitted to access this resource'
     //     }
-    //   });
+    //   })
     // }
   } catch(error) {
     return res.status(500).json({
       error: {
         message: 'An error occurred while getting user access. Please try again'
       }
-    });
+    })
   }
-};
+}
