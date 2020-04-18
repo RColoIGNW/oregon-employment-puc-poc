acc05776c4aff41dbc2ea6808de01da78a1bcfb4
desmondpnw3dev <pnw3dev@gmail.com>
2020-04-17 17:13:59 -0700

    remove unused firebase services

--- oed-server/src/util/firebase.ts
+++ oed-server/src/util/firebase.ts
@@ -2,6 +2,5 @@ import firebase from 'firebase-admin'
 export const firebaseApp = firebase.initializeApp({
   credential: firebase.credential.applicationDefault(),
 })
-export const auth = firebase.auth
 
 export default firebaseApp
