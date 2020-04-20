ed672a0e3cc24b0bf13dbc592341109c155b3ea9
desmondpnw3dev <pnw3dev@gmail.com>
2020-04-20 12:59:00 -0700

    catch errors

--- oed-server/src/services/applicants.ts
+++ oed-server/src/services/applicants.ts
@@ -7,19 +7,23 @@ import firebase from '../util/firebase'
 const db = firebase.firestore()
 
 export const getApplicants = async (_: Request, res: Response) => {
-  const applications: any = [] // TODO: add types
-  await db
-    .collection('users')
-    .orderBy('application')
-    .get()
-    .then(querySnapshot => {
-      querySnapshot.forEach(doc => {
-        applications.push(doc.data())
+  try {
+    const applications: any = [] // TODO: add types
+    await db
+      .collection('users')
+      .orderBy('application')
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          applications.push(doc.data())
+        })
       })
-    })
 
-  return res.send({
-    success: true,
-    response: applications
-  })
+    return res.status(200).send({
+      success: true,
+      response: applications
+    })
+  } catch (error) {
+    res.status(400).json({ error })
+  }
 }
