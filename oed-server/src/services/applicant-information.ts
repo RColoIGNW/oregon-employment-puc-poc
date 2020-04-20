80871f7ccaa35ec219bb32db66d42c2dd1f55b42
desmondpnw3dev <pnw3dev@gmail.com>
2020-04-20 09:45:02 -0700

    wip - fix transaction and remove auth check

--- oed-server/src/services/applicant-information.ts
+++ oed-server/src/services/applicant-information.ts
@@ -7,22 +7,24 @@ const db = firebase.firestore()
 
 export const submitApplicantInformation = async (req: Request, res: Response) => {
   const requestBody = req.body || {
+    uid: "123-fake-uid", // TODO: get from req.body.uid or add to request headers
     // add mock data
   }
-
-  const applicationsRef = db.collection('users').doc('some-uid').collection('pua-applications').doc()
-  const userRef = db.collection('users').doc('some-uid') // TODO: get from req.body.uid or add to request headers
+  const uid = requestBody.uid || '123'
+  const userRef = db.collection('users').doc(uid)
+  const applicationsRef = userRef.collection('pua-applications').doc()
 
   return db.runTransaction(async (t) => {
-      const userDoc = await t.get(userRef)
-      const { applicationCount } = userDoc.data()
-      t.update(userRef, { applicationCount: applicationCount ? applicationCount + 1 : 1 })
-      t.set(applicationsRef, requestBody)
-      return Promise.resolve('Transaction Successful!')
-    })
-    .then(() => res.status(200).json({ success: true}))
-    .catch(error => res.status(400).send(error))
-    .finally(() => logger('SubmitApplicationInformation Transaction Finished!'))
+    const userDoc = await t.get(userRef)
+    const applicationDoc = await t.get(applicationsRef)
+    const applicationCount = userDoc?.data()?.applicationCount
+    t[userDoc.data() ? 'update' :  'set'](userRef, { applicationCount: applicationCount ? applicationCount + 1 : 1 })
+    t[applicationDoc.data() ? 'update' : 'set'](applicationsRef, requestBody)
+    return Promise.resolve('Transaction Successful!')
+  })
+  .then(() => res.status(200).json({ success: true}))
+  .catch(error => res.status(400).send(error.toString()))
+  .finally(() => logger('SubmitApplicationInformation Transaction Finished!'))
 }
 
 export default submitApplicantInformation
