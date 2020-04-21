d9b040466efd922fcf7f753863d9ab9960f08470
desmondpnw3dev <pnw3dev@gmail.com>
2020-04-21 08:51:20 -0700

    added form api hook

---
+++ oed-client/src/hooks/useApplicantFormApi.ts
@@ -0,0 +1,31 @@
+import { request } from '../util/request'
+
+export default () => {
+
+  const getUnapprovedApplications = () => {
+    return request('http://localhost:4000/api/applications')
+    .then((result: any) => {
+      if (!result.success) { return [] }
+      return result.response
+    })
+    .catch(console.error)
+  }
+
+  const saveForm = (formData) => {
+    const body = JSON.stringify(formData)
+
+    const requestOptions = {
+      method: 'POST',
+      body,
+      redirect: 'follow',
+    }
+
+    return request('http://localhost:4000/api/new-application', requestOptions as any)
+      .catch((error) => console.log('error', error))
+  }
+
+  return {
+    saveForm,
+    getUnapprovedApplications,
+  }
+}
--- oed-client/src/pages/admin/approvals.tsx
+++ oed-client/src/pages/admin/approvals.tsx
@@ -1,9 +1,10 @@
 import { Grid } from "@material-ui/core"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 import ApprovalTable from '../../components/approval-table'
 import { Layout } from "../../components/layout"
 import { SEO } from "../../components/seo"
+import useApplicantFormApi from '../../hooks/useApplicantFormApi'
 import firebase from '../../lib/firebase'
 
 const useSignIn = () => { // fake for demo
@@ -12,7 +13,11 @@ const useSignIn = () => { // fake for demo
       if (!localStorage.token && typeof window !== 'undefined') {
         firebase.auth().signInWithEmailAndPassword('admin@ignw.test.com', 'Testing123!')
           .then(async () => {
-            localStorage.setItem('token', await firebase?.auth()?.currentUser?.getIdToken().catch(console.error) || '')
+            localStorage.setItem(
+              'token',
+              await firebase?.auth()?.currentUser?.getIdToken()
+               .catch(console.error) || ''
+            )
           })
           .catch(console.error)
       }
@@ -24,13 +29,25 @@ const useSignIn = () => { // fake for demo
 
 const InitialApplicationPage = () => {
   useSignIn()
+  const [tableData, setTableData] = useState([])
+  const { getUnapprovedApplications } = useApplicantFormApi()
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getUnapprovedApplications()
+      setTableData(data)
+    }
+
+    if (!tableData?.length) { getData() }
+    return () => {}
+  })
 
   return (
   <Layout>
     <SEO title={'Approvals'} />
     <Grid container direction="column" spacing={3} style={{marginTop: '2em'}}>
       <Grid item>
-        <ApprovalTable />
+        <ApprovalTable data={tableData}  />
       </Grid>
     </Grid>
   </Layout>
