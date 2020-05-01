fca318b0c51a6a0a9e39e2ada603a5be9f3cd9ac
desmondpnw3dev <pnw3dev@gmail.com>
2020-05-01 16:00:14 -0700

    remove unused declaration file

--- server/scripts/custom.d.ts
+++
@@ -1,7 +0,0 @@
-import pdfFormFill from "pdf-form-fill"
-
-declare module pdfFormFiil {
-  // import pdfFormFill from 'pdf-form-fill'
-// import pdfFormFiil from 'pdf-form-fill';
-  // export default pdfFormFill
-}
