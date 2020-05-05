529ed32d71edf8811cd95ad8a39d8ee0182ef414
Desmond G. Jones <pnw3dev@gmail.com>
2020-05-04 21:12:35 -0700

    test remote pdf download without logger enabled

--- server/src/util/logger.ts
+++ server/src/util/logger.ts
@@ -1,32 +1,32 @@
-import fs from 'fs'
+// import fs from 'fs'
+export default console
+// import bunyan, { LogLevel } from 'bunyan'
 
-import bunyan, { LogLevel } from 'bunyan'
+// const filePath =  process.env.NODE_ENV === 'production'
+// ? '/var/log/OregonState-PUA-BFF.log'
+// : `${__dirname}/../../logs/OregonState-PUA-BFF.log`
 
-const filePath =  process.env.NODE_ENV === 'production'
-? '/var/log/OregonState-PUA-BFF.log'
-: `${__dirname}/../../logs/OregonState-PUA-BFF.log`
-
-const logDir = `${__dirname}/../../logs/`
+// const logDir = `${__dirname}/../../logs/`
 
 // create directory if it doesn't exist
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir)
-}
+// if (!fs.existsSync(logDir)) {
+//   fs.mkdirSync(logDir)
+// }
 
-const log = bunyan.createLogger({
-  name: 'OregonState-PUA-BFF:App',
-  level: (process.env.LOG_LEVEL as LogLevel) || 'info',
-  streams: [
-    {
-      stream: process.stdout,
-    },
-    {
-      type: 'rotating-file',
-      path: filePath,
-      period: '1d', // daily rotation
-      count: 3, // keep 3 back copies
-    },
-  ],
+// const log = bunyan.createLogger({
+//   name: 'OregonState-PUA-BFF:App',
+//   level: (process.env.LOG_LEVEL as LogLevel) || 'info',
+//   streams: [
+//     {
+//       stream: process.stdout,
+//     },
+//     {
+//       type: 'rotating-file',
+//       path: filePath,
+//       period: '1d', // daily rotation
+//       count: 3, // keep 3 back copies
+//     },
+//   ],
 })
 
 export default log
