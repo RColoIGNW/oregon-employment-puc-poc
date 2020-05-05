7557efe666c80cf18a9e7f1e32f7d158f8cad313
desmondpnw3dev <pnw3dev@gmail.com>
2020-05-05 07:28:30 -0700

    pipe stream response

--- server/src/services/pdf-application.ts
+++ server/src/services/pdf-application.ts
@@ -1,3 +1,5 @@
+import fs from 'fs'
+
 import { Request, Response } from 'express'
 import fillPdf from 'fill-pdf'
 
@@ -7,7 +9,7 @@ import getMappings from '../util/mappings'
 const db = firebase.firestore()
 const pdfTemplatePath = __dirname + '/../templates/pdf-template.pdf'
 
-export const generatePdf = async (collectionName: string, req: Request, res: Response) => {
+export const generatePdf = (collectionName: string, req: Request, res: Response) => {
   return db
     .collection(collectionName)
     .doc(req.params.applicationId)
@@ -19,8 +21,14 @@ export const generatePdf = async (collectionName: string, req: Request, res: Res
         if (err) { throw new Error('Failed to generate pdf') }
         res.setHeader('Content-Type', 'application/pdf')
         res.setHeader('Content-disposition', 'attachment; filename=application.pdf')
-        res.write(output)
-        res.end()
+        fs.writeFile('./temp.pdf', output, err => {
+          if (err) {
+            console.error(err)
+            throw new Error('Failed to write temp file')
+          }
+        })
+        const file = fs.createReadStream('./temp.pdf');
+        file.pipe(res)
       })
     })
     .catch((error) => {
