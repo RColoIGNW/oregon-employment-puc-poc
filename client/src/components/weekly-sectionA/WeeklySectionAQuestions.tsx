acfce6e393219edda1a4ec69f3c105117370411e
aryeh <akempler-delugach@ignw.io>
2020-05-08 13:50:08 -0700

    added help buttons for each question

--- client/src/components/weekly-sectionA/WeeklySectionAQuestions.tsx
+++ client/src/components/weekly-sectionA/WeeklySectionAQuestions.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Grid from '@material-ui/core/Grid'
 import FormControl from "@material-ui/core/FormControl"
 import FormLabel from "@material-ui/core/FormLabel"
@@ -7,6 +7,9 @@ import FormControlLabel from "@material-ui/core/FormControlLabel"
 import Radio from "@material-ui/core/Radio"
 import questions from "../weekly-form/questions"
 import {WeeklySectionProps} from "../../models/WeeklySectionProps"
+import Button from "@material-ui/core/Button"
+import HelpIcon from "@material-ui/icons/Help"
+import HelpDialog from "../weekly-sectionC/HelpDialog"
 
 const defaultValue = {
   failedToAcceptOffer: false,
@@ -21,6 +24,16 @@ const defaultValue = {
 export default (props: WeeklySectionProps) => {
   const applicant = props.application || defaultValue
 
+  const [failedToAcceptExpanded, setFailedToAcceptExpanded] = useState(false)
+  const setFailed = (open: boolean) => {setFailedToAcceptExpanded(open)}
+
+  const [failedQuitExpanded, setQuitExpanded] = useState(false)
+  const setQuit = (open: boolean) => {setQuitExpanded(open)}
+
+  const [firedOrSuspendedExpanded, setFiredOrSuspendedExpanded] = useState(false)
+  const setFired = (open: boolean) => {setFiredOrSuspendedExpanded(open)}
+
+
   const { isDisabled } = props
   const disabled = !!isDisabled
 
@@ -34,6 +47,10 @@ export default (props: WeeklySectionProps) => {
         <Grid item xs={12} sm={7} md={6}>
           <FormControl component="fieldset">
             <FormLabel component="legend">{questions.failedToAcceptOffer}</FormLabel>
+            <Grid style={{display: 'flex', paddingTop: 10}}>
+              <Button variant={'outlined'} startIcon={<HelpIcon />} onClick={() => setFailedToAcceptExpanded(true)}>Help</Button>
+              {failedToAcceptExpanded && <HelpDialog title={'Question 1'} textSections={questions.failedToAcceptOfferHelp} openFn={setFailed}/>}
+            </Grid>
             <RadioGroup>
               <FormControlLabel
                 control={<Radio checked={applicant.failedToAcceptOffer === true} onChange={handleBooleanSelection} name="failedToAcceptOffer" value={true} disabled={disabled} />}
@@ -49,6 +66,10 @@ export default (props: WeeklySectionProps) => {
         <Grid item xs={12} sm={7} md={6}>
           <FormControl component="fieldset">
             <FormLabel component="legend">{questions.quitJob}</FormLabel>
+            <Grid style={{display: 'flex', paddingTop: 10}}>
+              <Button variant={'outlined'} startIcon={<HelpIcon />} onClick={() => setQuitExpanded(true)}>Help</Button>
+              {failedQuitExpanded && <HelpDialog title={'Question 2'} textSections={questions.quitJobHelp} openFn={setQuit}/>}
+            </Grid>
             <RadioGroup>
               <FormControlLabel
                 control={<Radio checked={applicant.quitJob === true} onChange={handleBooleanSelection} name="quitJob" value={true} disabled={disabled} />}
@@ -64,6 +85,10 @@ export default (props: WeeklySectionProps) => {
         <Grid item xs={12} sm={7} md={6}>
           <FormControl component="fieldset">
             <FormLabel component="legend">{questions.firedOrSuspended}</FormLabel>
+            <Grid style={{display: 'flex', paddingTop: 10}}>
+              <Button variant={'outlined'} startIcon={<HelpIcon />} onClick={() => setFiredOrSuspendedExpanded(true)}>Help</Button>
+              {firedOrSuspendedExpanded && <HelpDialog title={'Question 3'} textSections={questions.firedOrSuspendedHelp} openFn={setFired}/>}
+            </Grid>
             <RadioGroup>
               <FormControlLabel
                 control={<Radio checked={applicant.firedOrSuspended === true} onChange={handleBooleanSelection} name="firedOrSuspended" value={true} disabled={disabled} />}
--- client/src/components/weekly-sectionB/WeeklySectionBQuestions.tsx
+++ client/src/components/weekly-sectionB/WeeklySectionBQuestions.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from "react"
 import Grid from '@material-ui/core/Grid'
 import FormControl from "@material-ui/core/FormControl"
 import FormLabel from "@material-ui/core/FormLabel"
@@ -7,6 +7,9 @@ import FormControlLabel from "@material-ui/core/FormControlLabel"
 import Radio from "@material-ui/core/Radio"
 import questions from "../weekly-form/questions"
 import { WeeklySectionProps } from "../../models/WeeklySectionProps"
+import Button from "@material-ui/core/Button"
+import HelpIcon from "@material-ui/icons/Help"
+import HelpDialog from "../weekly-sectionC/HelpDialog"
 
 const defaultValue = {
   ableToWork: true,
@@ -28,11 +31,21 @@ export default (props: WeeklySectionProps) => {
 
   }
 
+  const [awayFromResidenceExpanded, setAwayFromResidenceExpanded] = useState(false)
+  const setAway = (open: boolean) => {setAwayFromResidenceExpanded(open)}
+
+  const [ableToWorkExpanded, setAbleToWorkExpanded] = useState(false)
+  const setAble = (open: boolean) => {setAbleToWorkExpanded(open)}
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} sm={7} md={6}>
         <FormControl component="fieldset">
           <FormLabel component="legend">{questions.awayFromResidence}</FormLabel>
+          <Grid style={{display: 'flex', paddingTop: 10}}>
+            <Button variant={'outlined'} startIcon={<HelpIcon />} onClick={() => setAwayFromResidenceExpanded(true)}>Help</Button>
+            {awayFromResidenceExpanded && <HelpDialog title={'Question 1'} textSections={questions.awayFromResidenceHelp} openFn={setAway}/>}
+          </Grid>
           <RadioGroup>
             <FormControlLabel
               control={<Radio checked={applicant.awayFromResidence === true} onChange={handleBooleanSelection} name="awayFromResidence" value={true} disabled={disabled} />}
@@ -48,6 +61,10 @@ export default (props: WeeklySectionProps) => {
       <Grid item xs={12} sm={7} md={6}>
         <FormControl component="fieldset">
           <FormLabel component="legend">{questions.ableToWork}</FormLabel>
+          <Grid style={{display: 'flex', paddingTop: 10}}>
+            <Button variant={'outlined'} startIcon={<HelpIcon />} onClick={() => setAbleToWorkExpanded(true)}>Help</Button>
+            {ableToWorkExpanded && <HelpDialog title={'Question 2'} textSections={questions.ableToWorkHelp} openFn={setAble}/>}
+          </Grid>
           <RadioGroup>
             <FormControlLabel
               control={<Radio checked={applicant.ableToWork === true} onChange={handleBooleanSelection} name="ableToWork" value={true} disabled={disabled} />}
