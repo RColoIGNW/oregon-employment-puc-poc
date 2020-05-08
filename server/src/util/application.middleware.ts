61bdfac379727a130eb0677168d758dfdff09caf
desmondpnw3dev <pnw3dev@gmail.com>
2020-05-08 14:34:56 -0700

    remove branch

--- server/src/util/application.middleware.ts
+++ server/src/util/application.middleware.ts
@@ -10,18 +10,12 @@ export const validateApplicationRequest: RequestHandler = async (
   res: Response,
   next: NextFunction,
 ) => {
-  try {
-    const ajv = new Ajv()
-    ajv.validate(schema, req.body)
-    log.info('application schema validation check:', JSON.stringify(ajv.errors))
-    if (ajv.errors?.length) {
-      log.error(ajv.errors)
-      // throw new Error(JSON.stringify(ajv.errors))
-      return res.status(400).send(ajv.errors)
-    }
-    next()
-  } catch (error) {
-    log.error(error)
-    res.status(400).send(error)
+  const ajv = new Ajv()
+  ajv.validate(schema, req.body)
+  log.info('application schema validation check:', JSON.stringify(ajv.errors))
+  if (ajv.errors?.length) {
+    log.error(ajv.errors)
+    return res.status(400).send(ajv.errors)
   }
+  next()
 }
