1e01756ce71b719057e85e8e3a6d654f33bcd0bd
desmondpnw3dev <pnw3dev@gmail.com>
2020-05-11 22:41:18 -0700

    add delays

--- client/cypress/integration/new-application.test.js
+++ client/cypress/integration/new-application.test.js
@@ -25,28 +25,35 @@ describe('New Application Page Smoke/E2E Testing', () => {
     // enter firstName --> next
     cy.get("input[name='firstName']").type('First Name e2e').should('have.value', 'First Name e2e')
     cy.get("[data-testid='next-button']").click()
+    cy.wait(200)
 
     // add employment record button --> name of employer --> accept button --> Next
     cy.get("[data-testid='next-button']").click()
+    cy.wait(200)
 
     // checkbox -> next
     cy.get("[data-testid='C_1']").click()
     cy.get("[data-testid='next-button']").click()
+    cy.wait(200)
 
     // checkbox --> text field --> next
     cy.get("[data-testid='D_1']").click()
     cy.get("[data-testid='next-button']").click()
+    cy.wait(200)
 
     // checkbox --> next
     cy.get("[data-testid='E_1']").click()
     cy.get("[data-testid='next-button']").click()
+    cy.wait(200)
 
     // assert on dropzone element --> next
     cy.get("[data-testid='next-button']").click()
+    cy.wait(200)
 
     // checkbox -> full name --> agree chexkbox --> submit application button
     cy.get("[data-testid='F_1']").click()
     cy.get("[data-testid='next-button']").click()
+    cy.wait(200)
 
     // assert url should include /application-submitted
     cy.url().should('include', '/application-submitted')
