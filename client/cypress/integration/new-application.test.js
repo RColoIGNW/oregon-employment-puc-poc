d83bcb60659b2438868ffca08859c2a7f16d3eb7
desmondpnw3dev <pnw3dev@gmail.com>
2020-05-11 23:20:38 -0700

    split up new application e2e steps

--- client/cypress/integration/new-application.test.js
+++ client/cypress/integration/new-application.test.js
@@ -6,50 +6,50 @@ describe('New Application Page Smoke/E2E Testing', () => {
   // afterAll(() => {
     // cleanup applications from db
   // })
-  beforeEach(() => {
+  it('should navigate to the new applications form page', () => {
     Cypress.env('REACT_APP_API_HOST', 'https://oed-poc-server-ohkmuktm2a-uw.a.run.app')
     const email = 'me@you.com'
     const password = 'Testing' // get from environment variable or mock auth call
     cy.visit('/')
-    cy.get('input[name=email]').type(email).should('have.value', email)
+    cy.get('input[name=email]').type(email)
     cy.get('input[name=password]').type(`${password}`)
     cy.get('button[type=submit]').click()
-  })
-  it('should navigate to the new applications form page', () => {
     cy.get("[data-testid='new-claim-link']").click()
     cy.url().should('include', '/application')
   })
-  it('should create and submit a new application', () => {
-    cy.get("[data-testid='new-claim-link']").click()
-
-    // enter firstName --> next
+  it('section A - should enter a first name and save progress', () => {
     cy.get("input[name='firstName']").type('First Name e2e').should('have.value', 'First Name e2e')
     cy.get("[data-testid='next-button']").click()
     cy.wait(200)
-
+  })
+  it('section B - should add employee record and save progress', () => {
     // add employment record button --> name of employer --> accept button --> Next
     cy.get("[data-testid='next-button']").click()
     cy.wait(200)
-
-    // checkbox -> next
+  });
+  it('section C - should select an answer checkbox and save progress', () => {
     cy.get("[data-testid='C_1']").click()
     cy.get("[data-testid='next-button']").click()
     cy.wait(200)
-
+  });
+  it('section D - should select an answer checkbox and save progress', () => {
     // checkbox --> text field --> next
     cy.get("[data-testid='D_1']").click()
     cy.get("[data-testid='next-button']").click()
     cy.wait(200)
-
-    // checkbox --> next
+  });
+  it('section E - should select an answer checkbox and save progress', () => {
     cy.get("[data-testid='E_1']").click()
     cy.get("[data-testid='next-button']").click()
     cy.wait(200)
-
+  });
+  it('section F - have the file upload (react dropzone) and save progress', () => {
     // assert on dropzone element --> next
     cy.get("[data-testid='next-button']").click()
     cy.wait(200)
+  })
 
+  it('section G - should select an answer, click agree and submit the application', () => {
     // checkbox -> full name --> agree chexkbox --> submit application button
     cy.get("[data-testid='F_1']").click()
     cy.get("[data-testid='next-button']").click()
@@ -57,7 +57,9 @@ describe('New Application Page Smoke/E2E Testing', () => {
 
     // assert url should include /application-submitted
     cy.url().should('include', '/application-submitted')
+  })
 
+  it('application submitted - should download a pdf application and navigate to view claim status.', () => {
     // download form button --> view applications button --> assert url should include /claim-status
     cy.get("[data-testid='download-application-button']").click()
     cy.get("[data-testid='view-claims-button']").click()
